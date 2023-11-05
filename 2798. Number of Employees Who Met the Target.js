99e5bea264a4eeb4ece3af62d122f5ae5b29e4a4
Zeinab Mohy <95243799+Zeinab-Mohy@users.noreply.github.com>
2023-11-05 11:47:23 +0200

    Create 2798. Number of Employees Who Met the Target.js

---
+++ 2798. Number of Employees Who Met the Target.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+var numberOfEmployeesWhoMetTarget = function(hours, target) {
+    let c=0;
+    for(let i=0;i<hours.length;i++){
+        if(hours[i]>=target){
+            c++;
+        }
+    }
+    return c;
+};
