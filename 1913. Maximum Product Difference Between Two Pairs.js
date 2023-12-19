f5ba080ba8d6a463825961832716e9f0d728d07a
Zeinab Mohy <95243799+Zeinab-Mohy@users.noreply.github.com>
2023-12-19 16:18:39 +0200

    Create 1913. Maximum Product Difference Between Two Pairs.js

---
+++ 1913. Maximum Product Difference Between Two Pairs.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    nums=nums.sort((a,b)=>b-a);
+    let ans=(nums[0]*nums[1])-(nums[nums.length-2]*nums[nums.length-1]);
+    return ans;
+};
